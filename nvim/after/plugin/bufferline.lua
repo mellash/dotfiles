2c5ae66ad749e435c503296597e4a689d57a6c7a
mellash <katamellash@gmail.com>
2024-12-04 00:16:19 -1000

    Made neo-tree as a buffer and changed to buffer instead of tab

--- nvim/after/plugin/bufferline.lua
+++ nvim/after/plugin/bufferline.lua
@@ -1,17 +1,26 @@
-require("bufferline").setup{
+require("bufferline").setup {
   options = {
     show_buffer_icons = true,
     show_close_icon = false,
     always_show_bufferline = true,
     auto_toggle_bufferline = true,
-    mode = "tabs",
+    mode = "buffers",
     numbers = "none",
+    separator_style = "slant",
+    persist_buffer_sort = true,
+    hover = {
+      enabled = true,
+      delay = 200,
+      reveal = { 'close' }
+    },
+    sort_by = 'insert_after_current',
     offsets = {
       {
-        filetype = "NeoTree",
+        filetype = "neo-tree",
         text = "File Explorer",
         text_align = "center",
-        separator = false,
+        highlight = 'Directory',
+        separator = true,
       },
     },
   },
