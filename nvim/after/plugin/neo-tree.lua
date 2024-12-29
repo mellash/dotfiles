a590e36117401471c464696507e6b0e7871dbd90
mellash <katamellash@gmail.com>
2024-12-28 23:36:32 -1000

    Add folder and diagnostics icons

--- nvim/after/plugin/neo-tree.lua
+++ nvim/after/plugin/neo-tree.lua
@@ -1,10 +1,35 @@
 require('neo-tree').setup({
   window = {
     position = "left",
-    width = 30,
+    width = 25,
     mapping_options = {
       noremap = true,
       nowait = true,
     }
   },
+  default_component_configs = {
+    icon = {
+      folder_closed = "", -- ▷▶
+      folder_open = "", -- ▽▼
+      folder_empty = "󰜌",
+    },
+    modified = {
+      symbol = "⬤",
+      highlight = "NeoTreeModified",
+    },
+    diagnostics = {
+      symbols = {
+        hint = "󰌶",
+        info = "",
+        warn = "󰀪",
+        error = "󰅚",
+      },
+      highlight = {
+        hint = "DiagnosticSignHint",
+        info = "DiagnosticSignInfo",
+        warn = "DiagnosticSignWarn",
+        error = "DiagnosticSignError",
+      }
+    },
+  }
 })
